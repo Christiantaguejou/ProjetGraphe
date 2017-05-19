de97f977b91138033301f6a833d45f6a02b8a97e
Christian TAGUEJOU <christiantaguejou@yahoo.fr>
2017-05-19 17:15:18 +0200

    Lecture du fichier .csv

--- Graphe/src/Commune.java
+++ Graphe/src/Commune.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by chris on 19/05/2017.
  */
@@ -16,4 +20,34 @@ public class Commune {
         this.longitude = longitude;
         this.latitude = latitude;
     }
+
+    public static void readFile(File file) {
+
+        List<String> result = new ArrayList<String>();
+
+        FileReader fr = null;
+        try {
+                fr = new FileReader(file);
+
+            BufferedReader br = new BufferedReader(fr);
+
+            //Tant que la ligne n'est pas nulle, on continue la lecture du fichier
+            for (String line = br.readLine(); line != null; line = br.readLine()) {
+                result.add(line);
+                System.out.println(line);
+            }
+
+            br.close();
+            fr.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Fichier Introuvable");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Problème lors de la lecture du fichier");
+            e.printStackTrace();
+        }
+      //  return result;
+    }
+
 }
--- Graphe/src/Main.java
+++ Graphe/src/Main.java
@@ -1,9 +1,23 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Created by Fabien on 19/05/2017.
  */
 public class Main {
-    
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+    /*    List<String> fichier = Commune.readFile(new File("CommunesFrance.csv"));
+
+        for (Iterator iter = listeNom.iterator(); iter.hasNext();)
+        {
+            String  ch2= (String)iter.next();
+            System.out.println(ch2)  ;
+        }*/
+
+    Commune.readFile(new File("CommunesFrance.csv"));
     }
 }
