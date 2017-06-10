eba2660080d33bccfc7c8b1aaae07b9402e1ccb4
MARK-PC\markk <markkpamy@gmail.com>
2017-06-10 11:13:01 +0200

    no message

--- Graphe/src/CsvCommunes.java
+++ Graphe/src/CsvCommunes.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -5,7 +6,7 @@ import java.util.List;
 /**
  * Created by Christian TAGUEJOU on 20/05/2017.
  */
-public class CsvCommunes{
+public class CsvCommunes {
 
     public static List<String> readFile(File file) {
 
@@ -35,36 +36,38 @@ public class CsvCommunes{
         return result;
     }
 
-    public static ArrayList<Commune> tableau(List<String> listeCommune){
+    public static ArrayList<Commune> tableau(List<String> listeCommune) {
 
         ArrayList<Commune> tabCommune = new ArrayList<>();
 
-        for(int n = 1; n < listeCommune.size(); n++){
-          //  System.out.println(listeCommune.get(n));
+        for (int n = 1; n < listeCommune.size(); n++) {
+            //  System.out.println(listeCommune.get(n));
             //Le ; permettra de découper les chaines de caracteres listeCommune.get(n)
             String[] commune = listeCommune.get(n).split(";");
-            tabCommune.add(new Commune(commune[0], commune[1], Integer.parseInt(commune[2]), Double.parseDouble(commune[3]),Double.parseDouble(commune[4])));
+            tabCommune.add(new Commune(commune[0], commune[1], Integer.parseInt(commune[2]), Double.parseDouble(commune[3]), Double.parseDouble(commune[4])));
         }
         return tabCommune;
     }
 
     /**
-     * Permet de creer une liste avec des communes contenant une population > popMin
+     * Permet de creer une liste avec des communes contenant une population >
+     * popMin
+     *
      * @param listeCommunes
      * @param popMin
      * @return
      */
-    public static ArrayList<Commune> triPopMin (ArrayList<Commune> listeCommunes, int popMin){
+    public static ArrayList<Commune> triPopMin(ArrayList<Commune> listeCommunes, int popMin) {
 
         ArrayList<Commune> listeTrie = new ArrayList<>();
 
-        for(int i = 0; i < listeCommunes.size(); i++) {
+        for (int i = 0; i < listeCommunes.size(); i++) {
             if (listeCommunes.get(i).getPopulation() > popMin) {
                 listeTrie.add(listeCommunes.get(i));
             }
         }
 
-       /* System.out.println("Liste triée popMin");
+        /* System.out.println("Liste triée popMin");
         for(Commune c : listeTrie){
             System.out.println(c.getPopulation());
         }*/
@@ -72,20 +75,46 @@ public class CsvCommunes{
     }
 
     /**
-     * Permet de creer une liste avec des communant ayant une population < popMax
-     * @param listeCommunes
+     * Permet de creer une liste avec des communes ayant une population < popMax
+     * @
+     *
+     * param listeCommunes
      * @param popMax
-     * @return
+     * @return une arraylist de communes
      */
-    public static ArrayList<Commune> triPopMax (ArrayList<Commune> listeCommunes, int popMax){
+    public static ArrayList<Commune> triPopMax(ArrayList<Commune> listeCommunes, int popMax) {
 
         ArrayList<Commune> listeTrie = new ArrayList<>();
 
-        for(int i = 0; i < listeCommunes.size(); i++) {
+        for (int i = 0; i < listeCommunes.size(); i++) {
             if (listeCommunes.get(i).getPopulation() > popMax) {
                 listeTrie.add(listeCommunes.get(i));
             }
         }
+        return listeTrie;
+    }
+
+    /**
+     * permet de creer un graphe avec des arcs de longueur inferieur a la
+     * distance a vol doiseau
+     *
+     * @param listeCommunes
+     * @param distanceMax
+     * @return une arraylist de communes
+     */
+    public static ArrayList<Commune> triVolDoiseauMax(ArrayList<Commune> listeCommunes, int distanceMax) {
+        ArrayList<Commune> listeTrie = new ArrayList<>();
+        return listeTrie;
+    }
+    /**
+     * permet de creer un graphe avec des arcs de longueur superieur a la
+     * distance a vol doiseau
+     * @param listeCommunes
+     * @param distanceMin
+     * @return 
+     */
+    public static ArrayList<Commune> triVolDoiseauMin(ArrayList<Commune> listeCommunes, int distanceMin) {
+        ArrayList<Commune> listeTrie = new ArrayList<>();
         return listeTrie;
     }
 }
