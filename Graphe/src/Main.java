1a2e16fb0df354830cf877aa49fffb9d84a786ae
Fabien Ogli <fabien.ogli@gmail.com>
2017-06-14 22:19:40 +0200

    predecesseur en + pour mark

--- Graphe/src/Main.java
+++ Graphe/src/Main.java
@@ -21,40 +21,44 @@ public class Main {
         List<String> liste;
         liste = CsvCommunes.readFile(new File("../doc/CommunesFrance.csv"));
         ArrayList<Commune> listeCommunes = CsvCommunes.tableau(liste);
-        ArrayList<Commune> graphe = Graphe.triPopMin(listeCommunes, 200000);
-        for(Commune c : graphe){
-            System.out.println(c.getNom());
+        Graphe graphe = new Graphe(listeCommunes, Graphe.triPar.POPULATION, Graphe.choixTri.MIN, 200000);
+        for(Arc arc : graphe.getArcs()){
+            System.out.println();
         }
-        System.out.println("FIN dAFFICHAGE");
-        //Trie de la liste
-        //ArrayList<Commune> listeTrie;
-        //listeTrie = CsvCommunes.triPopMin(listeCommunes, 10000);
-
-
-        //Graphes
-        Commune rennes = new Commune("rennes","RENNES",207178,-1.68333,48.0833);
-        Commune brest = new Commune("brest", "BREST", 141303,-4.48333,48.4);
-        Commune ozan = new Commune("ozan", "OZAN",618,4.91667,49.3833);
-        Commune nice = new Commune("nice","NICE",343304,7.25,43.7);
-
-        //   Graphe graph1 = new Graphe(listeTrie, rennes, brest);
-       /* for(Commune c : graph1.gps()){
-         //   System.out.println(c.getNom());
-        }*/
-
-        //Aetoile
-        ArrayList<Sommet> listeSommet= new ArrayList<>();
-        for(Commune c : graphe){
-            listeSommet.add(new Sommet(c));
-        }
-        Sommet sRennes = new Sommet(rennes);
-        Sommet sBrest = new Sommet(brest);
-        Sommet snice = new Sommet(nice);
-       /* System.out.println(sOzan.coutTotal(sRennes, sBrest));
-        System.out.println(new Arc(sRennes.commune, sOzan.commune).distanceVolOiseau());
-        System.out.println(new Arc(sBrest.commune, sOzan.commune).distanceVolOiseau());
-        System.out.println(new Arc(sRennes.commune, sOzan.commune).distanceVolOiseau() + new Arc(sBrest.commune, sOzan.commune).distanceVolOiseau());*/
-         LinkedList<Sommet> s = Aetoile.algo(listeSommet, snice, sRennes);
-         Aetoile.AfficherAetoile(s);
+        //ArrayList<Commune> graphe = Graphe.triPopMin(listeCommunes, 200000);
+//        for(Commune c : graphe){
+//            System.out.println(c.getNom());
+//        }
+//        System.out.println("FIN dAFFICHAGE");
+//        //Trie de la liste
+//        //ArrayList<Commune> listeTrie;
+//        //listeTrie = CsvCommunes.triPopMin(listeCommunes, 10000);
+//
+//
+//        //Graphes
+//        Commune rennes = new Commune("rennes","RENNES",207178,-1.68333,48.0833);
+//        Commune brest = new Commune("brest", "BREST", 141303,-4.48333,48.4);
+//        Commune ozan = new Commune("ozan", "OZAN",618,4.91667,49.3833);
+//        Commune nice = new Commune("nice","NICE",343304,7.25,43.7);
+//
+//        //   Graphe graph1 = new Graphe(listeTrie, rennes, brest);
+//       /* for(Commune c : graph1.gps()){
+//         //   System.out.println(c.getNom());
+//        }*/
+//
+//        //Aetoile
+//        ArrayList<Sommet> listeSommet= new ArrayList<>();
+//        for(Commune c : graphe){
+//            listeSommet.add(new Sommet(c));
+//        }
+//        Sommet sRennes = new Sommet(rennes);
+//        Sommet sBrest = new Sommet(brest);
+//        Sommet snice = new Sommet(nice);
+//       /* System.out.println(sOzan.coutTotal(sRennes, sBrest));
+//        System.out.println(new Arc(sRennes.commune, sOzan.commune).distanceVolOiseau());
+//        System.out.println(new Arc(sBrest.commune, sOzan.commune).distanceVolOiseau());
+//        System.out.println(new Arc(sRennes.commune, sOzan.commune).distanceVolOiseau() + new Arc(sBrest.commune, sOzan.commune).distanceVolOiseau());*/
+//         LinkedList<Sommet> s = Aetoile.algo(listeSommet, snice, sRennes);
+//         Aetoile.AfficherAetoile(s);
     }
 }
